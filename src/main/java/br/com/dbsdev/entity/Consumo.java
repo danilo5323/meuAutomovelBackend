4aba8368d9d0508631e1458a5fe0ecbed872c1ec
Danilo <dbsdev54@gmail.com>
2020-04-30 00:02:19 -0300

    ajuste na classe de cosumo

--- src/main/java/br/com/dbsdev/entity/Consumo.java
+++ src/main/java/br/com/dbsdev/entity/Consumo.java
@@ -20,170 +20,189 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class Consumo implements Serializable {
 
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue
-  private Long id;
-  @JsonFormat(pattern = "dd/MM/yyyy")
-  private LocalDate dataAbastecimento;
-  private BigDecimal precoCombustivel;
-  private Double mediaConsumoParcial;
-  private Double mediaGeral10Dias;
-  private Double mediaConsumo;
-  private String tipoCombustivel;
-  private Double custoPorKm;
-  private BigDecimal custoTotal;
-  private String nomePosto;
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
+	private Long id;
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private LocalDate dataAbastecimento;
+	private BigDecimal precoCombustivel;
+	private Double quantidadeLitros;
+	private Double mediaConsumoParcial;
+	private Double mediaGeral10Dias;
+	private Double mediaConsumo;
+	private String tipoCombustivel;
+	private Double custoPorKm;
+	private Long odometroParcial;
+	private BigDecimal custoTotal;
+	private String nomePosto;
 
-  @JsonIgnore
-  @ManyToOne
-  @JoinColumn(name="veiculo_id")
-  private Veiculo veiculo;
-  
-  public Consumo() {
-    super();
-  }
- 
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "veiculo_id")
+	private Veiculo veiculo;
 
-  public Consumo(Long id, LocalDate dataAbastecimento, BigDecimal precoCombustivel, Double mediaConsumoParcial,
-      Double mediaGeral10Dias, Double mediaConsumo, String tipoCombustivel, Double custoPorKm, BigDecimal custoTotal,
-      String nomePosto, Veiculo veiculo) {
-    super();
-    this.id = id;
-    this.dataAbastecimento = dataAbastecimento;
-    this.precoCombustivel = precoCombustivel;
-    this.mediaConsumoParcial = mediaConsumoParcial;
-    this.mediaGeral10Dias = mediaGeral10Dias;
-    this.mediaConsumo = mediaConsumo;
-    this.tipoCombustivel = tipoCombustivel;
-    this.custoPorKm = custoPorKm;
-    this.custoTotal = custoTotal;
-    this.nomePosto = nomePosto;
-    this.veiculo = veiculo;
-  }
+	public Consumo() {
+		super();
+	}
 
+	public Consumo(Long id, LocalDate dataAbastecimento, BigDecimal precoCombustivel, Double mediaConsumoParcial,
+			Double mediaGeral10Dias, Double mediaConsumo, String tipoCombustivel, Double custoPorKm,
+			BigDecimal custoTotal, String nomePosto, Double quantidadeLitros, Long odometroParcial, Veiculo veiculo) {
+		super();
+		this.id = id;
+		this.dataAbastecimento = dataAbastecimento;
+		this.precoCombustivel = precoCombustivel;
+		this.mediaConsumoParcial = mediaConsumoParcial;
+		this.mediaGeral10Dias = mediaGeral10Dias;
+		this.mediaConsumo = mediaConsumo;
+		this.tipoCombustivel = tipoCombustivel;
+		this.custoPorKm = custoPorKm;
+		this.custoTotal = custoTotal;
+		this.nomePosto = nomePosto;
+		this.veiculo = veiculo;
+		this.quantidadeLitros = quantidadeLitros;
+		this.odometroParcial  = odometroParcial;
+	}
 
-  public Veiculo getVeiculo() {
-    return veiculo;
-  }
+	
+	public Long getOdometroParcial() {
+		return odometroParcial;
+	}
 
-  public Consumo setVeiculo(Veiculo veiculo) {
-    this.veiculo = veiculo;
-    return this;
-  }
+	public void setOdometroParcial(Long odometroParcial) {
+		this.odometroParcial = odometroParcial;
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Double getQuantidadeLitros() {
+		return quantidadeLitros;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setQuantidadeLitros(Double quantidadeLitros) {
+		this.quantidadeLitros = quantidadeLitros;
+	}
 
-  public LocalDate getDataAbastecimento() {
-    return dataAbastecimento;
-  }
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
 
-  public void setDataAbastecimento(LocalDate dataAbastecimento) {
-    this.dataAbastecimento = dataAbastecimento;
-  }
+	public Consumo setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+		return this;
+	}
 
-  public BigDecimal getPrecoCombustivel() {
-    return precoCombustivel;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setPrecoCombustivel(BigDecimal precoCombustivel) {
-    this.precoCombustivel = precoCombustivel;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public Double getMediaConsumoParcial() {
-    return mediaConsumoParcial;
-  }
+	public LocalDate getDataAbastecimento() {
+		return dataAbastecimento;
+	}
 
-  public void setMediaConsumoParcial(Double mediaConsumoParcial) {
-    this.mediaConsumoParcial = mediaConsumoParcial;
-  }
+	public void setDataAbastecimento(LocalDate dataAbastecimento) {
+		this.dataAbastecimento = dataAbastecimento;
+	}
 
-  public Double getMediaGeral10Dias() {
-    return mediaGeral10Dias;
-  }
+	public BigDecimal getPrecoCombustivel() {
+		return precoCombustivel;
+	}
 
-  public void setMediaGeral10Dias(Double mediaGeral10Dias) {
-    this.mediaGeral10Dias = mediaGeral10Dias;
-  }
+	public void setPrecoCombustivel(BigDecimal precoCombustivel) {
+		this.precoCombustivel = precoCombustivel;
+	}
 
-  public Double getMediaConsumo() {
-    return mediaConsumo;
-  }
+	public Double getMediaConsumoParcial() {
+		return mediaConsumoParcial;
+	}
 
-  public void setMediaConsumo(Double mediaConsumo) {
-    this.mediaConsumo = mediaConsumo;
-  }
+	public void setMediaConsumoParcial(Double mediaConsumoParcial) {
+		this.mediaConsumoParcial = mediaConsumoParcial;
+	}
 
-  public String getTipoCombustivel() {
-    return tipoCombustivel;
-  }
+	public Double getMediaGeral10Dias() {
+		return mediaGeral10Dias;
+	}
 
-  public void setTipoCombustivel(String tipoCombustivel) {
-    this.tipoCombustivel = tipoCombustivel;
-  }
+	public void setMediaGeral10Dias(Double mediaGeral10Dias) {
+		this.mediaGeral10Dias = mediaGeral10Dias;
+	}
 
-  public Double getCustoPorKm() {
-    return custoPorKm;
-  }
+	public Double getMediaConsumo() {
+		return mediaConsumo;
+	}
 
-  public void setCustoPorKm(Double custoPorKm) {
-    this.custoPorKm = custoPorKm;
-  }
+	public void setMediaConsumo(Double mediaConsumo) {
+		this.mediaConsumo = mediaConsumo;
+	}
 
-  public BigDecimal getCustoTotal() {
-    return custoTotal;
-  }
+	public String getTipoCombustivel() {
+		return tipoCombustivel;
+	}
 
-  public void setCustoTotal(BigDecimal custoTotal) {
-    this.custoTotal = custoTotal;
-  }
+	public void setTipoCombustivel(String tipoCombustivel) {
+		this.tipoCombustivel = tipoCombustivel;
+	}
 
-  public String getNomePosto() {
-    return nomePosto;
-  }
+	public Double getCustoPorKm() {
+		return custoPorKm;
+	}
 
-  public void setNomePosto(String nomePosto) {
-    this.nomePosto = nomePosto;
-  }
+	public void setCustoPorKm(Double custoPorKm) {
+		this.custoPorKm = custoPorKm;
+	}
 
-  @Override
-  public String toString() {
-    return "Consumo [id=" + id + ", dataAbastecimento=" + dataAbastecimento + ", precoCombustivel=" + precoCombustivel
-        + ", mediaConsumoParcial=" + mediaConsumoParcial + ", mediaGeral10Dias=" + mediaGeral10Dias + ", mediaConsumo="
-        + mediaConsumo + ", tipoCombustivel=" + tipoCombustivel + ", custoPorKm=" + custoPorKm + ", custoTotal="
-        + custoTotal + ", nomePosto=" + nomePosto + ", veiculo=" + veiculo + "]";
-  }
+	public BigDecimal getCustoTotal() {
+		return custoTotal;
+	}
 
+	public void setCustoTotal(BigDecimal custoTotal) {
+		this.custoTotal = custoTotal;
+	}
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    return result;
-  }
+	public String getNomePosto() {
+		return nomePosto;
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Consumo other = (Consumo) obj;
-    if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
-    return true;
-  }
+	public void setNomePosto(String nomePosto) {
+		this.nomePosto = nomePosto;
+	}
+
+	@Override
+	public String toString() {
+		return "Consumo [id=" + id + ", dataAbastecimento=" + dataAbastecimento + ", precoCombustivel="
+				+ precoCombustivel + ", mediaConsumoParcial=" + mediaConsumoParcial + ", mediaGeral10Dias="
+				+ mediaGeral10Dias + ", mediaConsumo=" + mediaConsumo + ", tipoCombustivel=" + tipoCombustivel
+				+ ", custoPorKm=" + custoPorKm + ", custoTotal=" + custoTotal + ", nomePosto=" + nomePosto
+				+ ", veiculo=" + veiculo + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Consumo other = (Consumo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
